79a5cec9fa55913cbb9671782388b1963833b5d1
vosi23 <vasyle30@gmail.com>
2024-08-27 17:26:09 +0300

    Section12: GET and PUT examples
    Progress: 1/2

--- section-12/src/app/places/places.service.ts
+++ section-12/src/app/places/places.service.ts
@@ -1,20 +1,44 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 
 import { Place } from './place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PlacesService {
+export class PlacesService
+{
+  private httpClient= inject(HttpClient); 
   private userPlaces = signal<Place[]>([]);
 
   loadedUserPlaces = this.userPlaces.asReadonly();
 
-  loadAvailablePlaces() {}
+  loadAvailablePlaces()
+  {
+    return this.fetchPlaces('http://localhost:3000/places', 'Something went wrong, please check again later!');
+  }
 
-  loadUserPlaces() {}
+  loadUserPlaces()
+  {
+    return this.fetchPlaces('http://localhost:3000/user-places', 'Something went wrong, please check again later!');
+  }
 
-  addPlaceToUserPlaces(place: Place) {}
+  addPlaceToUserPlaces(placeId: string)
+  {
+    return this.httpClient.put('http://localhost:3000/user-places', {
+      placeId
+    });
+  }
 
   removeUserPlace(place: Place) {}
+
+  private fetchPlaces(url: string, errorMessage: string)
+  {
+    return this.httpClient.get<{places: Place[]}>(url)
+    .pipe(
+      map((resData) => resData.places),
+      catchError((error) => throwError(() => new Error(errorMessage)))
+    )
+  } 
 }
--- section-12/src/app/places/user-places/user-places.component.ts
+++ section-12/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +12,41 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit
+{
+  places = signal<Place[] | undefined>(undefined);
+  isFetching= signal(false);
+  error= signal('');
+
+  private placesService= inject(PlacesService);
+
+  // the http requests are done on angular by using special services
+  
+  private destroyRef= inject(DestroyRef);
+
+
+  ngOnInit(): void
+  {
+    this.isFetching.set(true);
+    const subscription= this.placesService.loadUserPlaces()
+    .subscribe({
+      next: (places) => 
+      {
+        // console.log(resData);
+        this.places.set(places);
+        //this.error.set('Something went wrong fetching the available places. Please try again later');
+      },
+      error: (error) =>
+      {
+        console.log(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      }
+    });
+
+    this.destroyRef.onDestroy(() =>
+      subscription.unsubscribe()
+    );
+  }
 }
